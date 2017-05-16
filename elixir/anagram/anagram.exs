0f0b678ab6ffa594eca2d9203098273634b84306
Andrei Luiz Nenevê <alneneve@gmail.com>
2017-05-16 17:36:52 +0000

    Anagram[SOLVED]

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,20 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-
+    _verify(base, candidates)
+  end
+  
+  defp _verify(base, candidates, actual \\ [])
+  defp _verify(_, [], actual), do: actual
+  defp _verify(base, [h|t], actual) do
+    if _same?(base, h) ,do: _verify(base, t, actual ++ [h]) ,else: _verify(base, t, actual)
+  end
+  
+  defp _same?(base, comp) do
+    cond do
+    String.downcase(base) == String.downcase(comp) -> false
+    String.downcase(base) |> String.graphemes |> Enum.sort |> Enum.join("") ==  String.downcase(comp) |> String.graphemes |> Enum.sort |> Enum.join("") -> true
+    true -> false
+    end
   end
 end
