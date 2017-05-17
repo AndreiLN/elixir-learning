62caf358a12aca7eb319cb4d7a72a56584e38751
Andrei Luiz Nenevê <alneneve@gmail.com>
2017-05-17 11:29:01 +0000

    Phone Number [SOLVED]

--- elixir/phone-number/phone_number.exs
+++ elixir/phone-number/phone_number.exs
@@ -18,8 +18,12 @@ defmodule Phone do
   """
   @spec number(String.t) :: String.t
   def number(raw) do
+    verify(raw)
   end
-
+  defp verify(raw) ,do: String.replace(raw,~r/[^0-9a-zA-Z]/,"") |> has_no_letter? 
+  defp has_no_letter?(raw) ,do: if String.match?(raw, ~r/[a-zA-Z]/) ,do: String.duplicate("0",10) ,else: valid_length?(raw)
+  defp valid_length?(raw) ,do: if String.match?(raw, ~r/^(1[0-9]{10})|^([0-9]{10})$/) ,do: cut(raw) ,else: String.duplicate("0",10)
+  defp cut(raw) ,do: if String.length(raw) == 10 ,do: raw ,else: String.replace(raw,~r/^1/,"")
   @doc """
   Extract the area code from a phone number
 
@@ -39,6 +43,8 @@ defmodule Phone do
   """
   @spec area_code(String.t) :: String.t
   def area_code(raw) do
+    verify(raw)
+    |> String.slice(0..2)
   end
 
   @doc """
@@ -60,5 +66,7 @@ defmodule Phone do
   """
   @spec pretty(String.t) :: String.t
   def pretty(raw) do
+    number = verify(raw)
+    "(#{String.slice(number, 0..2)}) #{String.slice(number, 3..5)}-#{String.slice(number, 6..10)}"
   end
 end
