73d2f4988af8e1888884691b96a1b16c13768c55
Andrei Luiz Neneve <andreiluizn@gmail.com>
2017-11-01 16:51:03 +0000

    Angular2 [Binding and Interpolation]

---
+++ angular/data-binding/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+
+import { AlertModule } from 'ngx-bootstrap';
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AlertModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
