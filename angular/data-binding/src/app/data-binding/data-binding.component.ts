32be7e3a46081ac3cd37126483d2b3334e64fe6a
Andrei Luiz Neneve <andreiluizn@gmail.com>
2017-11-03 12:38:42 +0000

    Angular2 [Data Binding]

--- angular/data-binding/src/app/data-binding/data-binding.component.ts
+++ angular/data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,27 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  //styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
   styles: [
   `
   .highlight {
       background-color: yellow;
       font-weight: bolder;
   }
-  `  
-  ]
+  `]
 })
 export class DataBindingComponent implements OnInit {
-  url: string = 'https://github.com/AndreiLN/elixir-learning';
-  urlImagem: string = 'http://lorempixel.com/400/200/city/';
-  cursoAngular: boolean = true;
-  
-  valorAtual: string = "";
-  valorSalvo: string = "";
-  
-  isMouseOver: boolean = false;
-  
-  nomeCurso: string = "Angular";
+  url = 'https://github.com/AndreiLN/elixir-learning';
+  urlImagem = 'http://lorempixel.com/400/200/city/';
+  cursoAngular = true;
+  valorAtual = "";
+  valorSalvo = "";
+  isMouseOver = false;
+  nomeCurso = "Angular";
+  valorInicial = 15;
   
   getValor(){ return 1; }
   
@@ -43,6 +40,10 @@ export class DataBindingComponent implements OnInit {
   
   ouMouseOverOut(){
     this.isMouseOver = !this.isMouseOver
+  }
+  
+  onMudouValor(evento){
+    console.log(evento.novoValor);
   }
   
   constructor() { }
