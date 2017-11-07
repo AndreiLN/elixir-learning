2a5fdcf952a5159f2c7c7bdee4b0730be6ee1855
Andrei Luiz Neneve <andreiluizn@gmail.com>
2017-11-07 16:22:57 +0000

    Angular2

---
+++ angular/servicos/src/app/cursos/cursos.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CursosService {
+    constructor() { }
+    getCursos() {
+        return ['Angular2', 'Java', 'Phonegap'];
+    }
+}
